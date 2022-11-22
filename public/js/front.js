27677afe124bc4bcabba6bb4992e3a1af26e3ddb
francesco.miceli <francescomiceli@outlook.com>
2022-11-22 17:16:25 +0100

    Init

--- public/js/front.js
+++ public/js/front.js
@@ -49833,7 +49833,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/francescomiceli/Desktop/Github/laravel-api/resources/js/front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! /Users/francescomiceli/Desktop/Github/laravel-boolpress/resources/js/front.js */"./resources/js/front.js");
 
 
 /***/ })
